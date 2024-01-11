de9b5a069539e7d48c5fdc5b1770bf9f19d5b195
terenceDev <terence.arseneault@hotmail.com>
2024-01-11 11:51:07 -0400

    fixed issue where if no live site, shows no site instead of populating the current site in new tab

--- components/smCard.jsx
+++ components/smCard.jsx
@@ -11,14 +11,21 @@ const SmCard = (props) => {
           src={props.image}
           alt="Image of whats for dinner app"
         ></Image>
-        <div className="bg-gray-900 p-4 rounded-b-2xl">
+        <div className="bg-gray-800 p-4 rounded-b-2xl">
           <h2 className="text-white">{props.title}</h2>
           <a target="_blank" href={props.repoLink}>
             <p className="text-gray-400">Github Repo</p>
           </a>
-          <a target="_blank" href={props.deployLink}>
-            <p className="text-gray-400">Live Site</p>
-          </a>
+
+          {props.deployLink ? (
+            <a target="_blank" href={props.deployLink}>
+              <p className="text-gray-400">Live Site</p>
+            </a>
+          ) : (
+            <a className="text-gray-400">
+              Live Site: <span className="text-red-600">N/A</span>
+            </a>
+          )}
         </div>
       </section>
     </div>
